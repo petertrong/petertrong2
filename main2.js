35c5f24c854dd3c4fb2e6f8797a38b1f963f509d
Nguyễn Trọng Đức <126842954+petertrong@users.noreply.github.com>
2023-12-18 22:37:03 +0700

    Update main2.js

--- main2.js
+++ main2.js
@@ -3,6 +3,8 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs
 document.addEventListener('DOMContentLoaded', function(){
   var card = documentElementById("card");
   var cardIcon = documnet.querySelector(".bx-card-alt");
+  var cardTitle = this.documentElement("card2");
+  var cardIcon2 = document.querySelector(".btn");
 
   cardIcon.onclick = function(){
     card.style.display = "block";
@@ -15,6 +17,28 @@ document.addEventListener('DOMContentLoaded', function(){
 
 }
 )
+
+  document.getElementById('loginBtn').addEventListener('click', function() {
+    // Thêm lớp 'loading' vào nút khi nhấp
+    this.classList.add('loading');
+
+    // Thêm lớp 'active' vào Newton's Cradle khi nhấp
+    document.getElementById('newtonsCradle').classList.add('active');
+
+    // Tạm dừng hiệu ứng trong khoảng thời gian animation (1.2s trong trường hợp này)
+    setTimeout(function() {
+      // Chuyển hướng đến trang login (hoặc thực hiện các hành động khác)
+      window.location.href = "login.html";
+
+      // Xóa lớp 'loading' khỏi nút sau khi chuyển hướng
+      document.getElementById('loginBtn').classList.remove('loading');
+      
+      // Xóa lớp 'active' khỏi Newton's Cradle
+      document.getElementById('newtonsCradle').classList.remove('active');
+    }, 1200); // 1200 milliseconds là thời gian của hiệu ứng Newton's Cradle
+  });
+
+
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
